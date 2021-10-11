ae61ab5a825e5921aa24161e0a56e34c18f53d07
BeggiNerd00 <bjartmar.hallgrimsson@gmail.com>
2021-10-11 11:41:41 +0000

    Notes. Video_6

---
+++ Notes, from video recordings/Notes. Video_6.dart
@@ -0,0 +1,73 @@
+import 'dart:io';
+
+import 'package:untitled3/untitled3.dart' as untitled3;
+
+void main(List<String> arguments) {
+
+  int num1 = 12;
+
+  int num2 = 50;
+
+  int total = 15+50;
+
+  calculate_area(num1, num2);
+
+}
+
+void calculate_area(int number1, int number2) {
+
+  int total = number1 * number2;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+  print('What is your name?: ');
+  String name = stdin.readLineSync();
+
+  print('How old are you?');
+  int age = int.parse(stdin.readLineSync());
+
+  print_name_and_age(name, age);
+
+
+
+}
+
+
+void print_name_and_age(String s, int i) {
+
+  print("Reading identity please hold... \nYou're name is $s, and you're $i years of age.");
+}
+
+
+
+
+
+
+
+
+
+void userInfo () {
+
+  print('What is your name?: ');
+  String name = stdin.readLineSync();
+
+  print('How old are you?');
+  int age = int.parse(stdin.readLineSync());
+
+  print('Hello $name, you are only $age years old.... nice!');
+}
+
+ */
